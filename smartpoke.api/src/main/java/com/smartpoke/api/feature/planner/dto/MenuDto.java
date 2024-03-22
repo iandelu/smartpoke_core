4f8fa2d9946084448fb141113da79cddddfe608e
Anedell <48674592+iandelu@users.noreply.github.com>
2024-03-22 18:22:20 +0100

    delete planner

--- smartpoke.api/src/main/java/com/smartpoke/api/feature/planner/dto/MenuDto.java
+++
@@ -1,5 +0,0 @@
-package com.smartpoke.api.feature.planner.dto;
-
-public class MenuDto {
-    
-}
--- smartpoke.api/src/main/java/com/smartpoke/api/feature/planner/repository/MenuRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smartpoke.api.feature.planner.repository;
-
-import com.smartpoke.api.feature.planner.model.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
