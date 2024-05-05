44ac444040d57940df739712e3511c8fb7e898a0
Anedell <48674592+iandelu@users.noreply.github.com>
2024-05-05 16:41:05 +0200

    Model Menu init

---
+++ smartpoke.api/src/main/java/com/smartpoke/api/feature/menu/model/Menu.java
@@ -0,0 +1,14 @@
+package com.smartpoke.api.feature.menu.model;
+
+import com.smartpoke.api.feature.user.model.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Menu {
+    private Date date;
+    private User user;
+    private ArrayList<MenuDay> menuDays;
+
+}
---
+++ smartpoke.api/src/main/java/com/smartpoke/api/feature/menu/model/TypeOfMeal.java
@@ -0,0 +1,5 @@
+package com.smartpoke.api.feature.menu.model;
+
+public enum TypeOfMeal {
+    BREAKFAST, LUNCH, DINNER, SNACK
+}
